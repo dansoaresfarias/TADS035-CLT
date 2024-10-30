18c47a20365523710ae48d573c6f4cf37ea0b12a
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-10-30 00:40:42 +0000

    Aula JS

---
+++ Aulas_JS/js/curriculoBT.js
@@ -0,0 +1,8 @@
+function hackear(params) {
+    let elementoNome = document.getElementById("nome");
+    elementoNome.innerHTML = "Nayara Peixoto Pires";
+
+    document.getElementById("foto").src = "img/nayara.jpg";
+    document.getElementById("menuDanilo").innerHTML = "Nayara Peixoto";
+    document.getElementById("apresentacao").style.backgroundColor = "#e36cbe";
+}
