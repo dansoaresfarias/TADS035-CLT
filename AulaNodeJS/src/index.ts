087bf50443b9807ce71579ad538c85506b305265
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-12-12 21:59:03 -0300

    NodeJS

--- AulaNodeJS/src/index.ts
+++ AulaNodeJS/src/index.ts
@@ -1,5 +1,26 @@
 //console.log("TADS035 vai vencer! Escutei um amém?!");
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import Routes from "./routes/routes";
 import { AppDataSource } from "./db/data-source";
+
+export default class Server {
+    constructor(app: Application) {
+        this.config(app);
+        new Routes(app);
+    }
+
+    private config(app: Application): void {
+        const corsOptions: CorsOptions = {
+            origin: "http://localhost:8081"
+        };
+
+        app.use(cors(corsOptions));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+    }
+}
+
 AppDataSource.initialize()
     .then(() => {
         // here you can start to work with your database
--- AulaNodeJS/src/models/genero.ts
+++ AulaNodeJS/src/models/genero.ts
@@ -8,7 +8,8 @@ export class Genero {
   @Column({ type: "varchar", length: 45, nullable: false })
   nome: string;
 
-  constructor(nome: string) {
+  constructor(nome: string, id?: number) {
+    if(id) this.idGenero = id;
     this.nome = nome;
   }
 }
